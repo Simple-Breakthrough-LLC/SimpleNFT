662684a93b609318a29e47e86e00475817296728
Nickolai Whitlow <ananasviolet@fintmcwells.com>
2022-06-08 12:29:13 -0700

    Solana connection and wallet (broken polyfills)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
-import React from "react";
+import { useMemo } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import ModalsProvider from "./contexts/Modals";
+
+import { clusterApiUrl } from '@solana/web3.js';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { WalletDialogProvider } from '@solana/wallet-adapter-material-ui';
 
 import BaseView from "./components/BaseView";
 import { Home } from "./views/Home";
@@ -26,7 +31,23 @@ const App = () => {
 };
 
 const Providers = ({ children }) => {
-  return <ModalsProvider>{children}</ModalsProvider>;
+    const network = process.env.REACT_APP_SOLANA_NETWORK;
+    const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+    const wallets = useMemo(
+        () => [
+            new PhantomWalletAdapter(),
+        ],
+        [network],
+    );
+
+    return <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletDialogProvider>
+          <ModalsProvider>{children}</ModalsProvider>;
+        </WalletDialogProvider>
+      </WalletProvider>
+    </ConnectionProvider>
 };
 
 export default App;
