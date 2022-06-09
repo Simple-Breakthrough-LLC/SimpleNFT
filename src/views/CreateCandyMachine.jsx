0be6a234200c42fcebf99bc4f7dad2ccb68de957
Nickolai Whitlow <ananasviolet@fintmcwells.com>
2022-06-09 16:03:56 -0700

    Moved instructions into utils. candyData validation.

--- src/views/CreateCandyMachine.jsx
+++ src/views/CreateCandyMachine.jsx
@@ -1,25 +1,14 @@
 import { useConnection, useWallet } from '@solana/wallet-adapter-react';
 import { WalletDialogButton } from '@solana/wallet-adapter-material-ui';
 import {
-	MintLayout,
-	TOKEN_PROGRAM_ID,
-	ASSOCIATED_TOKEN_PROGRAM_ID,
-	createInitializeMintInstruction,
-	createAssociatedTokenAccountInstruction,
-	createMintToInstruction,
-	getAssociatedTokenAddress
-} from '@solana/spl-token';
-import {
-	createInitializeCandyMachineInstruction,
-	PROGRAM_ID as CANDY_MACHINE_PROGRAM_V2_ID
-} from '@metaplex-foundation/mpl-candy-machine';
-import {
-	candyAccountSize
+	checkCandyData,
+	createCandyMachineInstructions,
+	setCollectionInstructions,
 } from '../web3/utils.js';
 const web3 = require('@solana/web3.js');
 
 // TODO get this from metaplex
-const TOKEN_METADATA_PROGRAM_ID = new web3.PublicKey('metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s');
+// const TOKEN_METADATA_PROGRAM_ID = new web3.PublicKey('metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s');
 
 const uuidFromConfigPubkey = configAccount => configAccount.toBase58().slice(0, 6);
 
@@ -31,10 +20,6 @@ export const CreateCandyMachine = () => {
 		const payer = wallet.publicKey;
 		const candyMachine = web3.Keypair.generate();
 		const collectionMint = web3.Keypair.generate();
-		const associatedTokenAccount = await getAssociatedTokenAddress(
-			collectionMint.publicKey,
-			payer
-		);
 		// TODO Either generate the treasury wallet, or obtain from user
 		const treasuryWallet = new web3.PublicKey('7DFRtmhGzEG61TVfL4LjhFe7toah7E9pQtLFX4KYa8vq');
 		// let metadataPDA = await web3.PublicKey.findProgramAddress(
@@ -55,12 +40,11 @@ export const CreateCandyMachine = () => {
 		console.log('payer:', payer.toBase58())
 		console.log('candy machine:', candyMachine.publicKey.toBase58())
 		console.log('collection mint:', collectionMint.publicKey.toBase58())
-		console.log('associated token account:', associatedTokenAccount.toBase58())
 
 		const candyData = {
-			uuid: uuidFromConfigPubkey(candyMachine.publicKey), // shouldn't it be 110000 or 01H333 or 94coJY or something...?
+			uuid: uuidFromConfigPubkey(candyMachine.publicKey), // shouldn't it be 110000 or 01H333 or something...?
 			price: 0.1e9,
-			symbol: "TEST", // TODO limit is 10 bytes
+			symbol: "TEST",
 			sellerFeeBasisPoints: 550, // 5.5%
 			maxSupply: 1,
 			isMutable: true,
@@ -75,86 +59,45 @@ export const CreateCandyMachine = () => {
 				}
 			],
 			hiddenSettings: {
-				name: "Test Token ", // with number added, can be no more than 32 bytes
+				name: "Test Token ",
 				uri: "https://example.com/",
-				hash: "80a7b27fb7c83f4178bbedc1e2a3a506", // for later verification of token reveals (32 chars)
+				hash: "80a7b27fb7c83f4178bbedc1e2a3a506", // TODO for later verification of token reveals
 			},
 			whitelistMintSettings: null,
 			itemsAvailable: 1,
 			gatekeeper: null,
 		};
-		const candyMachineSize = candyAccountSize(candyData.itemsAvailable);
-		const initInstructions = [
-			web3.SystemProgram.createAccount({
-				fromPubkey: payer,
-				newAccountPubkey: candyMachine.publicKey,
-				space: candyMachineSize,
-				lamports: await connection.getMinimumBalanceForRentExemption(candyMachineSize),
-				programId: CANDY_MACHINE_PROGRAM_V2_ID,
-			}),
-			createInitializeCandyMachineInstruction(
-				{
-					candyMachine: candyMachine.publicKey,
-					wallet: treasuryWallet,
-					authority: payer,
-					payer
-				},
-				{
-					data: candyData
-				}
-			),
-		];
+		const status = checkCandyData(candyData).err;
+		if (status.err) {
+			alert(status.err);
+			return;
+		}
 
-		const setCollectionInstructions = [
-			web3.SystemProgram.createAccount({
-				fromPubkey: payer,
-				newAccountPubkey: collectionMint.publicKey,
-				space: MintLayout.span,
-				lamports: await connection.getMinimumBalanceForRentExemption(MintLayout.span),
-				programId: TOKEN_PROGRAM_ID,
-			}),
-			createInitializeMintInstruction(
-				collectionMint.publicKey,
-				0,
+		const initInstructions = await createCandyMachineInstructions(
+			connection,
+			{
 				payer,
+				candyMachine,
+				treasuryWallet,
+			},
+			candyData,
+		);
+		const collectionInstructions = await setCollectionInstructions(
+			connection,
+			{
 				payer,
-				TOKEN_PROGRAM_ID,
-			),
-			createAssociatedTokenAccountInstruction(
-				payer,
-				associatedTokenAccount,
-				payer,
-				collectionMint.publicKey,
-			),
-			createMintToInstruction(
-				collectionMint.publicKey,
-				associatedTokenAccount,
-				payer,
-				1,
-				[],
-				TOKEN_PROGRAM_ID,
-			),
-			// Token Metadata Program: Create Metadata Accounts v2 (collection metadata, collection mint, owner, owner, owner)
-			// Token Metadata Program: V3 Create Master Edition (collection master edition, collection mint, owner * 3, collection metadata)
-			// Candy Machine: Set Collection (candy machine, owner, collection PDA, owner, collection metadata, collection mint, collection master edition, collection authority record)
-		];
+				collectionMint,
+			},
+		);
 
-		const instructions = [...initInstructions]//, ...setCollectionInstructions];
+		const instructions = [...collectionInstructions];// initInstructions
 		const transaction = new web3.Transaction();
 		transaction.add(...instructions);
-		console.log(instructions)
-		console.log(instructions[1].programId.toBase58())
-		// const signature = await wallet.sendTransaction(transaction, connection);
 		const signature = await wallet.sendTransaction(
 			transaction,
 			connection,
-			{signers: [candyMachine]} // collectionMint
+			{signers: [collectionMint]} // candyMachine
 		);
-		// const signature = await web3.sendAndConfirmTransaction(
-		// 	connection,
-		// 	transaction,
-		// 	[candyMachine],
-		// );
 		console.log('signature', signature);
 		connection.onSignature(signature, (result, context) => {
 			if (result.err)
--- src/web3/utils.js
+++ src/web3/utils.js
@@ -1,7 +1,57 @@
 import {
+	MintLayout,
+	TOKEN_PROGRAM_ID,
+	createInitializeMintInstruction,
+	createAssociatedTokenAccountInstruction,
+	createMintToInstruction,
+	getAssociatedTokenAddress,
+} from '@solana/spl-token';
+import {
+	createInitializeCandyMachineInstruction,
+	PROGRAM_ID as CANDY_MACHINE_PROGRAM_V2_ID
+} from '@metaplex-foundation/mpl-candy-machine';
+import {
 	CONFIG_ARRAY_START_V2,
 	CONFIG_LINE_SIZE_V2,
 } from './constants.js';
+const web3 = require('@solana/web3.js');
+
+export const checkCandyData = (candyData) => {
+	if (!candyData.symbol)
+		return {err: "Symbol cannot be empty."}
+	if (candyData.symbol.length > 10)
+		return {err: "Max symbol length is 10 characters."}
+	if (candyData.hiddenSettings.name) {
+		const maxName = candyData.hiddenSettings.name + "#" + candyData.maxSupply;
+		if (maxName.length > 32)
+			return {err: "Some token names would exceed 32 characters."}
+	}
+	if (candyData.hiddenSettings) {
+		if (!candyData.hiddenSettings.name)
+			return {err: "Name cannot be empty."}
+		if (!candyData.hiddenSettings.uri)
+			return {err: "URI cannot be empty."}
+		if (!candyData.hiddenSettings.hash)
+			return {err: "Verification hash cannot be empty."}
+		if (candyData.hiddenSettings.hash.length != 32)
+			return {err: "Verification hash must be 32 bytes."}
+	}
+	if (!candyData.creators || candyData.creators.length == 0)
+		return {err: "Need at least one creator."}
+	for (const creator of candyData.creators) {
+		if (!creator.address)
+			return {err: "Creator is missing address."}
+		if (!creator.verified)
+			return {err: "Creator is missing verified."}
+		if (!creator.share)
+			return {err: "Creator is missing share."}
+	}
+	if (candyData.creators.length > 4)
+		return {err: "Maximum of 4 creators."}
+	if (candyData.creators.reduce((acc, curr) => acc + curr.share, 0) != 100)
+		return {err: "Creator shares should add up to 100."}
+	return {err: false}
+}
 
 export const candyAccountSize = (itemsAvailable) => {
 	itemsAvailable = Number(itemsAvailable);
@@ -12,4 +62,70 @@ export const candyAccountSize = (itemsAvailable) => {
 		8 +
 		2 * (Math.floor(itemsAvailable / 8) + 1)
 	);
+}
+
+export const createCandyMachineInstructions = async (connection, accounts, candyData) => {
+	const { payer, candyMachine, treasuryWallet } = accounts;
+	const candyMachineSize = candyAccountSize(candyData.itemsAvailable);
+	return [
+		web3.SystemProgram.createAccount({
+			fromPubkey: payer,
+			newAccountPubkey: candyMachine.publicKey,
+			space: candyMachineSize,
+			lamports: await connection.getMinimumBalanceForRentExemption(candyMachineSize),
+			programId: CANDY_MACHINE_PROGRAM_V2_ID,
+		}),
+		createInitializeCandyMachineInstruction(
+			{
+				candyMachine: candyMachine.publicKey,
+				wallet: treasuryWallet,
+				authority: payer,
+				payer
+			},
+			{
+				data: candyData
+			}
+		),
+	];
+}
+
+export const setCollectionInstructions = async (connection, accounts) => {
+	const { payer, collectionMint } = accounts;
+	const associatedTokenAccount = await getAssociatedTokenAddress(
+		collectionMint.publicKey,
+		payer
+	);
+	return [
+		web3.SystemProgram.createAccount({
+			fromPubkey: payer,
+			newAccountPubkey: collectionMint.publicKey,
+			space: MintLayout.span,
+			lamports: await connection.getMinimumBalanceForRentExemption(MintLayout.span),
+			programId: TOKEN_PROGRAM_ID,
+		}),
+		createInitializeMintInstruction(
+			collectionMint.publicKey,
+			0,
+			payer,
+			payer,
+			TOKEN_PROGRAM_ID,
+		),
+		createAssociatedTokenAccountInstruction(
+			payer,
+			associatedTokenAccount,
+			payer,
+			collectionMint.publicKey,
+		),
+		createMintToInstruction(
+			collectionMint.publicKey,
+			associatedTokenAccount,
+			payer,
+			1,
+			[],
+			TOKEN_PROGRAM_ID,
+		),
+		// Token Metadata Program: Create Metadata Accounts v2 (collection metadata, collection mint, owner, owner, owner)
+		// Token Metadata Program: V3 Create Master Edition (collection master edition, collection mint, owner * 3, collection metadata)
+		// Candy Machine: Set Collection (candy machine, owner, collection PDA, owner, collection metadata, collection mint, collection master edition, collection authority record)
+	];
 }
